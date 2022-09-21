e96698ff1f2eeda955147a6e3d41f09e19cae489
Max Meier <max@studiomax.io>
2022-09-20 20:21:47 -0700

    copy

--- contracts/NewKing.sol
+++ contracts/NewKing.sol
@@ -5,7 +5,7 @@ contract NewKing {
     constructor() payable {
         address king = 0xa14f59d9a2927a2cF96157CBD19759298eA6821f;
         (bool sent, ) = king.call{value: msg.value}("");
-        require(sent, "Failed to send Ether");
+        require(sent, "Failed to send ETH on deploy.");
     }
 
     fallback() external payable {
