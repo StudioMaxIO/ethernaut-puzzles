e0da42bd3e1355a6f42d0e44aea01e71386585fb
Max Meier <max@studiomax.io>
2022-09-23 09:47:31 -0700

    remove comment

--- scripts/gatekeeper1.js
+++ scripts/gatekeeper1.js
@@ -11,9 +11,6 @@ module.exports = async (callback) => {
     let entrant = await gatekeeper.entrant();
     console.log("Entrant:", entrant);
 
-    // use gas limit 24847 (learned from running contracts locally + findGas method)
-    // const gas = 24847;
-
     // start with gate key uint64(uint160(tx.origin))
     // then apply mask 0xFFFFFFFF0000FFFF
     const gateKey = await gatekeeperCaller.getKeyFromAddress(accounts[0]);
